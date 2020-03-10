ea7f39c04aa33807adcbaa6526fd6d417f715a22
Steven Judd <steven.judd@dvn.com>
2020-03-09 22:42:50 -0500

    converted script to use a param for url

--- ReturnUrlFromUrlDefense.ps1
+++ ReturnUrlFromUrlDefense.ps1
@@ -1,11 +1,21 @@
-#this script is intended to run after copying the URL to the clipboard from a defended email
-#the next line is for testing
-#Set-Clipboard -Value "https://urldefense.com/v3/__https://info.databricks.com/dc/iqoSFi8gRxN3bjGZEDn90EjHxqOFCR2NzPdiA0JFHdX0IdwPlwSu4ZUIkP-L8dy-S3IhJ8vFXu5rT4cyemtY8kUaEFQuX2p5XNG0zsfYbUPW2oy8syn06YHS3nsbeGmc/CMS00V0Kh0PY0CoP0U0rS0r__"
+param (
+    [Parameter(Position = 0)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Url #= "https://urldefense.com/v3/__https://info.databricks.com/dc/iqoSFi8gRxN3bjGZEDn90EjHxqOFCR2NzPdiA0JFHdX0IdwPlwSu4ZUIkP-L8dy-S3IhJ8vFXu5rT4cyemtY8kUaEFQuX2p5XNG0zsfYbUPW2oy8syn06YHS3nsbeGmc/CMS00V0Kh0PY0CoP0U0rS0r__"
+) 
 
-#get URL from clipboard
-$url = Get-Clipboard
-if (-not($url)) {
-    Write-Error "Nothing in the clipboard. Please copy the URL Defense string to parse into the clipboard before running the script."
-    return
+if ($Url) {
+    $UrlWithDelimiter = ($Url | Select-String "__.*__" -OutVariable SearchResult).Matches.Value
+    #remove the delimiters at the beginning and end of the Url
+    if ($UrlWithDelimiter.Substring(0, 2) -eq "__") {
+        $UrlWithDelimiter = $UrlWithDelimiter.Substring(2)
+    }
+    if ($UrlWithDelimiter.Substring($UrlWithDelimiter.Length - 2) -eq "__") {
+        ($UrlWithDelimiter).SubString(0, ($UrlWithDelimiter).Length - 2)
+    }
 }
-($url | Select-String "__.*__" -OutVariable SearchResult | Select-Object -ExpandProperty "Matches").Value.SubString(2, ($SearchResult | Select-Object -ExpandProperty Matches).Length - 4)
+
+# examples:
+# C:\Users\judds\git\github\stevenjudd\sjParseUrlDefense\ReturnUrlFromUrlDefense.ps1 -Url "https://urldefense.com/v3/__https://info.databricks.com/dc/iqoSFi8gRxN3bjGZEDn90EjHxqOFCR2NzPdiA0JFHdX0IdwPlwSu4ZUIkP-L8dy-S3IhJ8vFXu5rT4cyemtY8kUaEFQuX2p5XNG0zsfYbUPW2oy8syn06YHS3nsbeGmc/CMS00V0Kh0PY0CoP0U0rS0r__"
+# C:\Users\judds\git\github\stevenjudd\sjParseUrlDefense\ReturnUrlFromUrlDefense.ps1 -Url ""
+# C:\Users\judds\git\github\stevenjudd\sjParseUrlDefense\ReturnUrlFromUrlDefense.ps1

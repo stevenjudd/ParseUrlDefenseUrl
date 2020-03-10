ed48fdc1ec5728e404ceb9c7a99c120eea6d4732
Steven Judd <steven.judd@dvn.com>
2020-03-09 22:59:37 -0500

    converted to a function and enhanced the error handling

--- ReturnUrlFromUrlDefense.ps1
+++ ReturnUrlFromUrlDefense.ps1
@@ -1,21 +1,29 @@
-param (
-    [Parameter(Position = 0)]
-    [ValidateNotNullOrEmpty()]
-    [string]$Url #= "https://urldefense.com/v3/__https://info.databricks.com/dc/iqoSFi8gRxN3bjGZEDn90EjHxqOFCR2NzPdiA0JFHdX0IdwPlwSu4ZUIkP-L8dy-S3IhJ8vFXu5rT4cyemtY8kUaEFQuX2p5XNG0zsfYbUPW2oy8syn06YHS3nsbeGmc/CMS00V0Kh0PY0CoP0U0rS0r__"
-) 
+function Get-OriginalUrlFromUrlDefense {
+    param (
+        [Parameter(Mandatory, Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Url
+    ) 
 
-if ($Url) {
-    $UrlWithDelimiter = ($Url | Select-String "__.*__" -OutVariable SearchResult).Matches.Value
-    #remove the delimiters at the beginning and end of the Url
-    if ($UrlWithDelimiter.Substring(0, 2) -eq "__") {
-        $UrlWithDelimiter = $UrlWithDelimiter.Substring(2)
+    if ($Url) {
+        $UrlWithDelimiter = ($Url | Select-String "__.*__" -OutVariable SearchResult).Matches.Value
+        if ($UrlWithDelimiter) {
+            #remove the delimiters at the beginning and end of the Url
+            if ($UrlWithDelimiter.Substring(0, 2) -eq "__") {
+                $UrlWithDelimiter = $UrlWithDelimiter.Substring(2)
+            }
+            if ($UrlWithDelimiter.Substring($UrlWithDelimiter.Length - 2) -eq "__") {
+                ($UrlWithDelimiter).SubString(0, ($UrlWithDelimiter).Length - 2)
+            }
+        }
+        else {
+            Write-Error "Url value does not contain the proper double-underscore delimiters"
+        }
     }
-    if ($UrlWithDelimiter.Substring($UrlWithDelimiter.Length - 2) -eq "__") {
-        ($UrlWithDelimiter).SubString(0, ($UrlWithDelimiter).Length - 2)
-    }
-}
+} #end Get-sjUrlFromUrlDefense function
 
 # examples:
-# C:\Users\judds\git\github\stevenjudd\sjParseUrlDefense\ReturnUrlFromUrlDefense.ps1 -Url "https://urldefense.com/v3/__https://info.databricks.com/dc/iqoSFi8gRxN3bjGZEDn90EjHxqOFCR2NzPdiA0JFHdX0IdwPlwSu4ZUIkP-L8dy-S3IhJ8vFXu5rT4cyemtY8kUaEFQuX2p5XNG0zsfYbUPW2oy8syn06YHS3nsbeGmc/CMS00V0Kh0PY0CoP0U0rS0r__"
-# C:\Users\judds\git\github\stevenjudd\sjParseUrlDefense\ReturnUrlFromUrlDefense.ps1 -Url ""
-# C:\Users\judds\git\github\stevenjudd\sjParseUrlDefense\ReturnUrlFromUrlDefense.ps1
+# Get-OriginalUrlFromUrlDefense
+# Get-OriginalUrlFromUrlDefense -Url "https://urldefense.com/v3/__https://info.databricks.com/dc/iqoSFi8gRxN3bjGZEDn90EjHxqOFCR2NzPdiA0JFHdX0IdwPlwSu4ZUIkP-L8dy-S3IhJ8vFXu5rT4cyemtY8kUaEFQuX2p5XNG0zsfYbUPW2oy8syn06YHS3nsbeGmc/CMS00V0Kh0PY0CoP0U0rS0r__"
+# Get-OriginalUrlFromUrlDefense -Url ""
+# Get-OriginalUrlFromUrlDefense "blah"

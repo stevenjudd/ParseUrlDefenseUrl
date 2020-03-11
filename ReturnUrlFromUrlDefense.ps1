a047e74926fef783fc4cf8e488d4234718d24518
Steven Judd <steven.judd@dvn.com>
2020-03-11 00:16:59 -0500

    set script examples to relative paths

--- ReturnUrlFromUrlDefense.ps1
+++ ReturnUrlFromUrlDefense.ps1
@@ -16,6 +16,6 @@ if ($Url) {
 }
 
 # examples:
-# C:\Users\judds\git\github\stevenjudd\sjParseUrlDefense\ReturnUrlFromUrlDefense.ps1 -Url "https://urldefense.com/v3/__https://info.databricks.com/dc/iqoSFi8gRxN3bjGZEDn90EjHxqOFCR2NzPdiA0JFHdX0IdwPlwSu4ZUIkP-L8dy-S3IhJ8vFXu5rT4cyemtY8kUaEFQuX2p5XNG0zsfYbUPW2oy8syn06YHS3nsbeGmc/CMS00V0Kh0PY0CoP0U0rS0r__"
-# C:\Users\judds\git\github\stevenjudd\sjParseUrlDefense\ReturnUrlFromUrlDefense.ps1 -Url ""
-# C:\Users\judds\git\github\stevenjudd\sjParseUrlDefense\ReturnUrlFromUrlDefense.ps1
+# .\ReturnUrlFromUrlDefense.ps1 -Url "https://urldefense.com/v3/__https://info.databricks.com/dc/iqoSFi8gRxN3bjGZEDn90EjHxqOFCR2NzPdiA0JFHdX0IdwPlwSu4ZUIkP-L8dy-S3IhJ8vFXu5rT4cyemtY8kUaEFQuX2p5XNG0zsfYbUPW2oy8syn06YHS3nsbeGmc/CMS00V0Kh0PY0CoP0U0rS0r__"
+# .\ReturnUrlFromUrlDefense.ps1 -Url ""
+# .\ReturnUrlFromUrlDefense.ps1

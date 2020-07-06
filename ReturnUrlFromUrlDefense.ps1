e28b228b6b3c3156f3a4416e762174f958c745d5
Steven Judd <steven.judd@gmail.com>
2020-07-05 23:51:32 -0500

    updated url

--- ReturnUrlFromUrlDefense.ps1
+++ ReturnUrlFromUrlDefense.ps1
@@ -1,7 +1,7 @@
 param (
     [Parameter(Position = 0)]
     [ValidateNotNullOrEmpty()]
-    [string]$Url #= "https://urldefense.com/v3/__https://info.databricks.com/dc/iqoSFi8gRxN3bjGZEDn90EjHxqOFCR2NzPdiA0JFHdX0IdwPlwSu4ZUIkP-L8dy-S3IhJ8vFXu5rT4cyemtY8kUaEFQuX2p5XNG0zsfYbUPW2oy8syn06YHS3nsbeGmc/CMS00V0Kh0PY0CoP0U0rS0r__"
+    [string]$Url #= "https://urldefense.com/v3/__https://github.com/steven__judd/sjUrlDefense__;!!J9_hdUX_JbjuLQ!_mpQgRsIaawOILsUWxCcr5y3kFNhQir7iT8vEvcJzL8J4OfrN2b3ichHNbkMj4c$"
 ) 
 
 if ($Url) {
@@ -16,6 +16,6 @@ if ($Url) {
 }
 
 # examples:
-# .\ReturnUrlFromUrlDefense.ps1 -Url "https://urldefense.com/v3/__https://info.databricks.com/dc/iqoSFi8gRxN3bjGZEDn90EjHxqOFCR2NzPdiA0JFHdX0IdwPlwSu4ZUIkP-L8dy-S3IhJ8vFXu5rT4cyemtY8kUaEFQuX2p5XNG0zsfYbUPW2oy8syn06YHS3nsbeGmc/CMS00V0Kh0PY0CoP0U0rS0r__"
+# .\ReturnUrlFromUrlDefense.ps1 -Url "https://urldefense.com/v3/__https://github.com/steven__judd/sjUrlDefense__;!!J9_hdUX_JbjuLQ!_mpQgRsIaawOILsUWxCcr5y3kFNhQir7iT8vEvcJzL8J4OfrN2b3ichHNbkMj4c$"
 # .\ReturnUrlFromUrlDefense.ps1 -Url ""
 # .\ReturnUrlFromUrlDefense.ps1

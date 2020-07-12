120de23f07210dc7758ca73edaf7500bd1ea82ce
Steven Judd <steven.judd@gmail.com>
2020-07-12 15:21:26 -0500

    wordsmithing the comments Added membertype to Get-Member cmdlet

--- ConsoleCommand.ps1
+++ ConsoleCommand.ps1
@@ -1,9 +1,9 @@
 # an example URL protected by URL Defense
 $url = "https://urldefense.com/v3/__https://github.com/stevenjudd/sjUrlDefense__;!!J9_hdUX_JbjuLQ!_mpQgRsIaawOILsUWxCcr5y3kFNhQir7iT8vEvcJzL8J4OfrN2b3ichHNbkMj4c$"
-# making sure it can pull the URL from the variable using the double-underscore
+# making sure the URL is pulled from the variable using the double-underscore
 $url | sls "__.*?__"
 # check what is contained in the object
-$url | sls "__.*?__" | gm
+$url | sls "__.*?__" | gm -MemberType Property
 # looking inside the Matches property
 $url | sls "__.*?__" | select matches
 # whoops. It is an object. Need to expand the object
@@ -24,9 +24,9 @@ $url2 | sls "__.*?__"
 ($url2 | sls "__.*?__" | select -ExpandProperty matches).value
 # oops. There is also a problem with the RegEx. Need to fix that first.
 ($url2 | sls "__.*__" | select -ExpandProperty matches).value
-# now for the replace
+# now for the replace (and failure...)
+# it removes all of the double-underscores, which will break the URL
 ($url2 | sls "__.*__" | select -ExpandProperty matches).value -replace "__", ""
-# it removed all of the double-underscores, which will break the URL
 # to fix this, use substring to remove the first 2 and last 2 characters
 ($url2 | sls "__.*__" | select -ExpandProperty matches).value.substring(2, ($url2 | sls "__.*__" | select -ExpandProperty matches).length - 4)
 # or 
@@ -35,4 +35,4 @@ $url2 | sls "__.*?__"
 ($url2 | sls "__.*__" -OutVariable SearchResult | select -ExpandProperty matches).value.substring(2, ($SearchResult | select -ExpandProperty matches).length - 4)
 # or
 $SearchScope = $url2 | sls "__.*__" ; ($SearchScope | select -ExpandProperty matches).value.substring(2, ($SearchScope | select -ExpandProperty matches).length - 4)
-
+# The end

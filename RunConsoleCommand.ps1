2e4ce3227d76b124f51d0147d132479814d45952
Steven Judd <steven.judd@gmail.com>
2023-05-15 23:50:54 -0500

    Capture the Prediction Source Turn prediction off (distracting) Changed on-screen output to make comment vs cmd clear

--- RunConsoleCommand.ps1
+++ RunConsoleCommand.ps1
@@ -1,15 +1,23 @@
 $content = Get-Content .\ConsoleCommand.ps1
+if (-not ($global:PSReadLineOptionPredictionSource)) {
+  $global:PSReadLineOptionPredictionSource = (Get-PSReadLineOption).PredictionSource
+}
+Set-PSReadLineOption -PredictionSource None
 Clear-Host #please only use this cmdlet for demonstrations
 foreach ($line in $content) {
-    Write-Host "$line"
+  if ($line -match '^#') {
+    Write-Host "$line" -ForegroundColor Green
     Write-Host ''
-    if ($line -match "^#") {
-        Start-Sleep -Seconds 2
-    }
-    elseif ($line) {
-        Invoke-Expression -Command $line
-        Write-Host ' '
-        Wait-Debugger
-        Clear-Host
-    }
+    Start-Sleep -Seconds 2
+  } elseif ($line) {
+    Write-Host "PS> $line"
+    Write-Host ''
+    Invoke-Expression -Command $line | Out-Host
+    Write-Host ('=' * $Host.UI.RawUI.WindowSize.Width)
+    Write-Host ' '
+    Wait-Debugger
+    # $null = Read-Host 'Press Enter to continue'
+    Clear-Host
+  }
 }
+Set-PSReadLineOption -PredictionSource $PSReadLineOptionPredictionSource
